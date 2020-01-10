d96e91a841316b8af3b46e74ed5f6d72b97fac46
Pedro Del Moral Lopez <pdelmorallopez@gmail.com>
2020-01-10 14:49:43 -0500

    updated gitignore to avoid images being committed, updated book __str__

--- core/models.py
+++ core/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class Book(models.Model):
+    """ Book Django ORM Object
+    """
     title = models.CharField("Title", max_length=512)
     author = models.CharField("Author", max_length=128)
     description = models.CharField("Description", max_length=1024)
     picture = models.ImageField("Image")
     date_published = models.DateField("Date Published")
+
+    def __str__(self):
+        """ Returns Book Title and Author
+
+        Arguments: None
+        Returns: String
+        """
+        return f'{self.title} by {self.author}'
 
     def as_json(self):
         """ Converts a Django ORM object into a dict that can be passed to the frontend as JSON
